8f502a0ec49314f0108e657938d284095a89042b
Chugunov Roman <Zebs-BMK@yandex.ru>
2019-10-11 22:06:13 +0300

    Added handling of a invalid pathes

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,21 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::Request;
+
 #[get("/")]
 fn index() -> &'static str {
   "Hello Rocket!"
 }
 
+#[catch(404)]
+fn not_found(request: &Request) -> String {
+  format!("Path {} is not a valid path :C", request.uri())
+}
+
 fn main() {
-  rocket::ignite().mount("/", routes![index]).launch();
+  rocket::ignite()
+    .mount("/", routes![index])
+    .register(catchers![not_found])
+    .launch();
 }
